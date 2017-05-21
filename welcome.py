20fc69fde1c406c34cde9926aa623f1f9f8cdcd3
Sergio Flores <vitavitae1@gmail.com>
2017-05-21 17:47:50 +0200

    Añade compras

--- welcome.py
+++ welcome.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Copyright 2015 IBM Corp. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +15,8 @@
 # limitations under the License.
 
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, url_for, render_template
+from flask import request
 import pymongo
 from pymongo import MongoClient
 
@@ -21,15 +24,21 @@ connection = pymongo.MongoClient('ds147821.mlab.com', 47821)
 db = connection['scapd']
 db.authenticate('admin', '8hMzYgP5b2tDQoesP0Ge')
 
-#post = {"nombre": "Tomates", "precio": "2.4", "comprador": "Sergio"}
-#db.compras.insert_one(post).inserted_id
-
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-@app.route('/')
+@app.route('/index')
 def Welcome():
     return app.send_static_file('index.html')
 
+@app.route('/compras/consultadas')
+def formarQuery():
+	nombre = request.form['nombre']
+	precio = request.form['precio']
+	comprador = request.form['comprador']
+	query = {"nombre": nombre, "precio": precio, "comprador": comprador}
+	return QueryCompras(query)
+	
 def QueryCompras(query):
 	lista = []
 	compra = []
@@ -41,9 +50,8 @@ def QueryCompras(query):
 		compra = []
 	return jsonify(results=lista)
 
-@app.route('/compras')
 def GetCompras():
-    return QueryCompras("")
+    return QueryCompras(None)
 
 @app.route('/compras/comprador/<comprador>')
 def GetComprasParaComprador(comprador):
@@ -53,7 +61,25 @@ def GetComprasParaComprador(comprador):
 def GetComprasParaNombre(nombre):
     return QueryCompras({"nombre": nombre})
 
+@app.route("/compras/consultar")
+def consultarCompras():
+    return render_template("consultarCompras.html")	
+	
+@app.route("/compras/add")
+def addCompras():
+    return render_template("addCompras.html")
+ 
+@app.route("/compras/incluida", methods=['POST'])
+def compraIncluida():
+	nombre = request.form['nombre']
+	precio = request.form['precio']
+	comprador = request.form['comprador']
+	post = {"nombre": nombre, "precio": precio, "comprador": comprador}
+	db.compras.insert_one(post).inserted_id
+	return "Añadida la compra: {} por {}€ de {}".format(nombre, precio, comprador)
+ 	
+	
 port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=int(port))
-	#app.run(host='127.0.0.1', port=int(port))
+	#app.run(host='0.0.0.0', port=int(port))
+	app.run(host='127.0.0.1', port=int(port))
